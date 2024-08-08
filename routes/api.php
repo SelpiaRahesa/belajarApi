4e3faed5c1899c349710a5bd6562e9099f7c834c
SelpiaRahesa <selpiarahesa@gmail.com>
2024-08-08 11:10:58 +0700

    Api selpia terbaru

--- routes/api.php
+++ routes/api.php
@@ -18,21 +18,15 @@ Route::get('/user', function (Request $request) {
 // Route::get('kategori/{id}', [KategoriController::class, 'show']);
 // Route::put('kategori/{id}', [KategoriController::class, 'update']);
 // Route::delete('kategori/{id}', [KategoriController::class, 'destroy']);
-<<<<<<< HEAD
+
 // Route::middleware('auth:sanctum')->group(function () {
-=======
-Route::middleware('auth:sanctum')->group(function () {
->>>>>>> e75bd766dabb6c37ea2c906b44fabf25ab339978
-    Route::post('/logout', [\App\Http\Controllers\Api\AuthController::class, 'logout']);
+Route::post('/logout', [App\Http\Controllers\Api\AuthController::class, 'logout']);
 Route::resource('kategori', KategoriController::class);
 Route::resource('genre', GenreController::class);
 Route::resource('aktor', AktorController::class);
 Route::resource('film', FilmController::class);
 
-<<<<<<< HEAD
+
 // });
-=======
-});
->>>>>>> e75bd766dabb6c37ea2c906b44fabf25ab339978
 Route::post('login', [AuthController::class, 'login']);
 Route::post('register', [AuthController::class, 'register']);
